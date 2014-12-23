e537dd3f22b4fd3952c3492c795bf72372035b78
Vaclav Petras <wenzeslaus@gmail.com>
2014-12-22 23:40:12 -0500

    build html in selected subdirectories (1 level), script for increasing link depth by one (for these cases)

--- build-pages.sh
+++ build-pages.sh
@@ -18,6 +18,20 @@ do
     cat $FOOT_FILE >> $OUTDIR/$FILE
 done
 
+for DIR in grass arcgis resources project_titles
+do
+    mkdir -p $OUTDIR/$DIR
+
+    for FILE in `ls $DIR/*.html`
+    do
+        TGT_FILE=$OUTDIR/$DIR/`basename $FILE`
+        ./increase-file-depth.py < $HEAD_FILE > $TGT_FILE
+        echo "<!-- This is a generated file. Do not edit. -->" >> $TGT_FILE
+        ./strip-whitestace.py < $FILE >> $TGT_FILE
+        ./increase-file-depth.py < $FOOT_FILE >> $TGT_FILE
+    done
+done
+
 for FILE in *.css
 do
     cp --update $FILE $OUTDIR
