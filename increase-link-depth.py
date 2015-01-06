6536133720c2bdb4daf491ed815042437483896e
Vaclav Petras <wenzeslaus@gmail.com>
2015-01-06 16:56:49 -0500

    add missing file from f996a82 and fix image links in sub pages foot and head

--- increase-link-depth.py
+++ increase-link-depth.py
@@ -6,10 +6,12 @@ import re
 
 link_element = re.compile(r'link.+href="(.+)"')
 a_element = re.compile(r'a.+href="(.+)"')
+img_element = re.compile(r'img.+src="(.+)"')
 
 for line in fileinput.input():
     # ignore header
     if link_element.search(line) or a_element.search(line):
         line = line.replace('href="', 'href="../')
-
+    if img_element.search(line):
+        line = line.replace('src="', 'src="../')
     sys.stdout.write(line)
