74e76dacc10ef556b0a6ff957fc9cbd9840d6e09
Anna Petrasova <kratochanna@gmail.com>
2015-08-22 17:10:39 -0400

    add links to all modules mentioned in text

--- edit.py
+++ edit.py
@@ -8,6 +8,9 @@ block_start = re.compile(r'^\s*<pre><code>\s*$')
 
 for line in fileinput.input():
     # ignore header
+    line = re.sub(r'<em class="module">([^<]*)</em>',
+                  r'<em class="module"><a href="https://grass.osgeo.org/grass70/manuals/\1.html">\1</a></em>',
+                  line)
     if block_start.search(line):
         line = line.rstrip(' \t\n')
         sys.stdout.write(line)
