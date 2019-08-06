6a551846fcc2b4a7e3f15758cca807f084df7f04
Vaclav Petras <wenzeslaus@gmail.com>
2019-08-06 11:04:16 -0400

    Update version in URL

--- edit.py
+++ edit.py
@@ -9,7 +9,7 @@ block_start = re.compile(r'^\s*<pre ?.*><code ?.*>\s*$')
 for line in fileinput.input():
     # ignore header
     line = re.sub(r'<em class="module">([^<]*)</em>',
-                  r'<em class="module"><a href="https://grass.osgeo.org/grass74/manuals/\1.html">\1</a></em>',
+                  r'<em class="module"><a href="https://grass.osgeo.org/grass76/manuals/\1.html">\1</a></em>',
                   line)
     if block_start.search(line):
         line = line.rstrip(' \t\n')
